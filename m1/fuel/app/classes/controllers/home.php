9d6a30676505baf789dd565f47619f366ea3d0d8
Neira Piedrahita <jneira@columbia.cs.colostate.edu>
2022-03-23 17:43:28 -0600

    mod_home.php

--- m1/fuel/app/classes/controllers/home.php
+++ m1/fuel/app/classes/controllers/home.php
@@ -1,1 +1,12 @@
+<?php
 
+class Controller_m1 extends Controller_Template
+{
+    public function action_index(){
+
+        $data = array();
+        $this->template->title = 'Main Page';
+        $this->template->main_css=Asset::css('main.css');
+        $this->template->content=View::forge('m1/index.php',$data);
+    }
+}
