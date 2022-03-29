51804be3ae9ef9fbffd9d7c3b7b9504a4a19936b
Megan Booher <meganboo@rams.colostate.edu>
2022-03-29 13:53:40 -0600

    added homeage info, logos, fixed parameter ranges

--- m1/fuel/app/logs/2022/03/29.php
+++ m1/fuel/app/logs/2022/03/29.php
@@ -16,3 +16,16 @@ ERROR - 2022-03-29 10:33:50 --> Notice - Undefined variable: content in /s/bach/
 ERROR - 2022-03-29 11:06:06 --> Notice - Undefined variable: rows in /s/bach/m/under/meganboo/local_html/CS312_Term_Project/m1/fuel/app/views/template.php on line 17
 ERROR - 2022-03-29 11:22:45 --> Notice - Undefined variable: error in /s/bach/m/under/meganboo/local_html/CS312_Term_Project/m1/fuel/app/classes/controller/milestone.php on line 33
 ERROR - 2022-03-29 11:27:34 --> Notice - Undefined variable: colors in /s/bach/m/under/meganboo/local_html/CS312_Term_Project/m1/fuel/app/classes/controller/milestone.php on line 13
+ERROR - 2022-03-29 12:04:47 --> Notice - Undefined variable: content in /s/bach/m/under/meganboo/local_html/CS312_Term_Project/m1/fuel/app/views/template.php on line 26
+ERROR - 2022-03-29 12:07:00 --> Error - The requested view could not be found: template.php in /s/bach/m/under/meganboo/local_html/CS312_Term_Project/m1/fuel/core/classes/view.php on line 492
+ERROR - 2022-03-29 12:07:14 --> Error - The requested view could not be found: template.php in /s/bach/m/under/meganboo/local_html/CS312_Term_Project/m1/fuel/core/classes/view.php on line 492
+ERROR - 2022-03-29 12:07:26 --> Error - The requested view could not be found: template.php in /s/bach/m/under/meganboo/local_html/CS312_Term_Project/m1/fuel/core/classes/view.php on line 492
+ERROR - 2022-03-29 12:07:42 --> Error - The requested view could not be found: template.php in /s/bach/m/under/meganboo/local_html/CS312_Term_Project/m1/fuel/core/classes/view.php on line 492
+ERROR - 2022-03-29 12:08:13 --> Error - The requested view could not be found: template.php in /s/bach/m/under/meganboo/local_html/CS312_Term_Project/m1/fuel/core/classes/view.php on line 492
+ERROR - 2022-03-29 12:08:16 --> Error - The requested view could not be found: template.php in /s/bach/m/under/meganboo/local_html/CS312_Term_Project/m1/fuel/core/classes/view.php on line 492
+ERROR - 2022-03-29 12:08:41 --> Error - The requested view could not be found: template.php in /s/bach/m/under/meganboo/local_html/CS312_Term_Project/m1/fuel/core/classes/view.php on line 492
+ERROR - 2022-03-29 13:16:41 --> Fatal Error - Method Fuel\Core\View::__toString() must not throw an exception, caught ParseError: syntax error, unexpected '<', expecting end of file in /s/bach/m/under/meganboo/local_html/CS312_Term_Project/m1/fuel/core/classes/response.php on line 0
+ERROR - 2022-03-29 13:20:49 --> Error - syntax error, unexpected 'style' (T_STRING), expecting ',' or ')' in /s/bach/m/under/meganboo/local_html/CS312_Term_Project/m1/fuel/app/classes/controller/milestone.php on line 15
+ERROR - 2022-03-29 13:21:07 --> Error - syntax error, unexpected '=', expecting ',' or ')' in /s/bach/m/under/meganboo/local_html/CS312_Term_Project/m1/fuel/app/classes/controller/milestone.php on line 15
+ERROR - 2022-03-29 13:27:38 --> Error - Could not find asset: companylogo.jpg in /s/bach/m/under/meganboo/local_html/CS312_Term_Project/m1/fuel/core/classes/asset/instance.php on line 413
+ERROR - 2022-03-29 13:30:43 --> Error - Could not find asset: comanylogo.png in /s/bach/m/under/meganboo/local_html/CS312_Term_Project/m1/fuel/core/classes/asset/instance.php on line 413
--- m1/fuel/app/views/template.php
+++ m1/fuel/app/views/template.php
@@ -1,12 +1,20 @@
-<?php  echo Asset::css('main.css')?>
-
 <head>
+<?php echo Asset::css('index.css'); ?>
+
     <title><?php echo $title; ?> </title>
     <meta charset="utf-16">
         <meta name="author" content="CS312 milestone1">
         <meta name="description" content="Color Coordinate">
         <meta name="keywords" content="Homepage, HTML5, CS312, CSU">
 </head>
+<header>
+    <div id='CSU_logo'>
+        <?php echo Asset::img('csu_logo.png', array('style' => 'width: 450', 'alt' => 'csu logo'));?>
+    </div>
+    <div id="title">
+        <h2> <?php echo $title; ?></h2>
+    </div>
+</header>
 
 <nav>
     <ul id="top_menu">
@@ -15,14 +23,19 @@
         <li><a href="https://www.cs.colostate.edu/">CS Dept</a></li>
         <li><a href="https://www.online.colostate.edu/">CSU Online</a></li>
         <li><?php echo "<a href= 'https://www.cs.colostate.edu:4444/~meganboo/CS312_Term_Project/m1/index.php/milestone/color?n_rows_cols=$rows&n_colors=$colors'>Color Coordinate Page</a>" ?></li>
-
-
     </ul>
     <hr>
 </nav>
 
-<main>
-    <h1>Biography</h1>
-    <p> text </p>
 
-</main>
+
+<body>
+    <div id='logo'>
+        <?php echo Asset::img('companylogo.png', array('style' => 'width: 500px', 'alt' => 'company logos'));?>
+    </div>
+    <p>We believe that expecting mothers deserve as much support as possible regardless of species. At catmidwifefinder.com we are committed to match you to the ideal midwife for your feline companion in under 24 hours.</p>
+</body>
+
+<footer>
+CS312 Spring 2022
+</footer>
