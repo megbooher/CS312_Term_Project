28a5d5adf5a4bb351f163d3fec91b30719a57c2d
Megan Booher <meganboo@rams.colostate.edu>
2022-03-29 11:30:46 -0600

    added URL parameter and error messages

--- m1/fuel/app/logs/2022/03/29.php
+++ m1/fuel/app/logs/2022/03/29.php
@@ -8,3 +8,11 @@ ERROR - 2022-03-29 10:10:50 --> Error - Could not find asset: west.css in /s/bac
 ERROR - 2022-03-29 10:11:31 --> Error - Could not find asset: west.css in /s/bach/m/under/meganboo/local_html/CS312_Term_Project/m1/fuel/core/classes/asset/instance.php on line 413
 ERROR - 2022-03-29 10:22:12 --> Compile Error - Cannot declare class Controller_EastWest, because the name is already in use in /s/bach/m/under/meganboo/local_html/CS312_Term_Project/m1/fuel/app/classes/controller/milestone1.php on line 40
 ERROR - 2022-03-29 10:22:35 --> Compile Error - Cannot declare class Controller_Milestone, because the name is already in use in /s/bach/m/under/meganboo/local_html/CS312_Term_Project/m1/fuel/app/classes/controller/milestone1.php on line 40
+ERROR - 2022-03-29 10:31:12 --> Error - Could not find asset: east.css in /s/bach/m/under/meganboo/local_html/CS312_Term_Project/m1/fuel/core/classes/asset/instance.php on line 413
+ERROR - 2022-03-29 10:32:53 --> Error - Could not find asset: CSU-Official-wrdmrk-357-Rev.png in /s/bach/m/under/meganboo/local_html/CS312_Term_Project/m1/fuel/core/classes/asset/instance.php on line 413
+ERROR - 2022-03-29 10:33:32 --> Error - Could not find asset: CSU-Official-wrdmrk-357-Rev.png in /s/bach/m/under/meganboo/local_html/CS312_Term_Project/m1/fuel/core/classes/asset/instance.php on line 413
+ERROR - 2022-03-29 10:33:40 --> Notice - Undefined variable: content in /s/bach/m/under/meganboo/local_html/CS312_Term_Project/m1/fuel/app/views/template.php on line 20
+ERROR - 2022-03-29 10:33:50 --> Notice - Undefined variable: content in /s/bach/m/under/meganboo/local_html/CS312_Term_Project/m1/fuel/app/views/template.php on line 20
+ERROR - 2022-03-29 11:06:06 --> Notice - Undefined variable: rows in /s/bach/m/under/meganboo/local_html/CS312_Term_Project/m1/fuel/app/views/template.php on line 17
+ERROR - 2022-03-29 11:22:45 --> Notice - Undefined variable: error in /s/bach/m/under/meganboo/local_html/CS312_Term_Project/m1/fuel/app/classes/controller/milestone.php on line 33
+ERROR - 2022-03-29 11:27:34 --> Notice - Undefined variable: colors in /s/bach/m/under/meganboo/local_html/CS312_Term_Project/m1/fuel/app/classes/controller/milestone.php on line 13
--- m1/fuel/app/views/template.php
+++ m1/fuel/app/views/template.php
@@ -1,27 +1,28 @@
+<?php  echo Asset::css('main.css')?>
 
 <head>
     <title><?php echo $title; ?> </title>
     <meta charset="utf-16">
-    <meta name="author" content="CS312 milestone1">
-    <meta name="description" content="Color Coordinate">
-    <meta name="keywords" content="Homepage, HTML5, CS312, CSU">
-    
+        <meta name="author" content="CS312 milestone1">
+        <meta name="description" content="Color Coordinate">
+        <meta name="keywords" content="Homepage, HTML5, CS312, CSU">
 </head>
 
-<body>
-<header>
-    <div id="CSU_logo">
-    </div>
-    <div id="title">
-        <h2> <?php echo $title; ?></h2>
-    </div>
-</header>
+<nav>
+    <ul id="top_menu">
+        <li><a href="./index">My Homepage</a></li>
+        <li><a href="https://www.colostate.edu/">CSU Home</a></li>
+        <li><a href="https://www.cs.colostate.edu/">CS Dept</a></li>
+        <li><a href="https://www.online.colostate.edu/">CSU Online</a></li>
+        <li><?php echo "<a href= 'https://www.cs.colostate.edu:4444/~meganboo/CS312_Term_Project/m1/index.php/milestone/color?n_rows_cols=$rows&n_colors=$colors'>Color Coordinate Page</a>" ?></li>
 
-<br>
-<hr>
 
-<footer>
-    CS312 Colorado State University
-</footer>
-</body>
-</html>
+    </ul>
+    <hr>
+</nav>
+
+<main>
+    <h1>Biography</h1>
+    <p> text </p>
+
+</main>
