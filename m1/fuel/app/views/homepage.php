e460f57e5c384d6c3a89b8597164ff4c48d5bf0d
Andrew Ricciardi <namtab92@shark.cs.colostate.edu>
2022-03-30 13:01:23 -0600

    edited template.php, homepage.php and index.css

--- m1/fuel/app/views/homepage.php
+++ m1/fuel/app/views/homepage.php
@@ -1,4 +1,7 @@
+
 <div id='logo'>
     <?php echo Asset::img('companylogo.png', array('style' => 'width: 500px', 'alt' => 'company logos'));?>
 </div>
-<p>We believe that expecting mothers deserve as much support as possible regardless of species. At catmidwifefinder.com we are committed to match you to the ideal midwife for your feline companion in under 24 hours.</p>
+<p>We believe that expecting mothers deserve as much support as possible regardless of species. </p>
+
+<p>At catmidwifefinder.com we are committed to match you to the ideal midwife for your feline companion in under 24 hours.</p>
--- m1/fuel/app/views/template.php
+++ m1/fuel/app/views/template.php
@@ -8,9 +8,6 @@
         <meta name="keywords" content="Homepage, HTML5, CS312, CSU">
 </head>
 <header>
-    <div id='CSU_logo'>
-        <?php echo Asset::img('csu_logo.png', array('style' => 'width: 450', 'alt' => 'csu logo'));?>
-    </div>
     <div id="title">
         <h2> <?php echo $title; ?></h2>
     </div>
@@ -21,9 +18,6 @@
         <li><a href="./index">Homepage</a></li>
         <li><a href="./about">About</a></li>
         <li><?php echo "<a href= 'https://www.cs.colostate.edu:4444/~meganboo/CS312_Term_Project/m1/index.php/milestone/color?n_rows_cols=$rows&n_colors=$colors'>Color Coordinate Page</a>" ?></li>
-        <li><a href="https://www.colostate.edu/">CSU Home</a></li>
-        <li><a href="https://www.cs.colostate.edu/">CS Dept</a></li>
-        <li><a href="https://www.online.colostate.edu/">CSU Online</a></li>
     </ul>
     <hr>
 </nav>
