43ccd63996a61b976a94e36f36177d9327a2ef0f
MaggieFunston <maggie.funston@gmail.com>
2022-04-27 16:54:56 -0600

    make only get request

--- m1/fuel/app/classes/controller/milestone.php
+++ m1/fuel/app/classes/controller/milestone.php
@@ -26,45 +26,28 @@ class Controller_Milestone extends Controller_Template
         $colors = null;
         $rowError = null;
         $colorError = null;
-        $requestMethod = $_SERVER["REQUEST_METHOD"];
-        if ($requestMethod === "GET") {
-            if (isset($_GET["n_rows_cols"])) {
-                $rows = (int) Input::get("n_rows_cols");
-                if ($rows < 1 || $rows > 26) {
-                    $rowError = "n_rows_cols must be between 1 and 26!";
-                }
-            }
 
-            // load and check number of colors
-            if (isset($_GET["n_colors"])) {
-                $colors = (int) Input::get("n_colors");
-                if ($colors < 1 || $colors > 10) {
-                    $colorError = "n_colors must be between 1 and 10!";
-                }
-            }
-        } else {
-            if (isset($_POST["n_rows_cols"])) {
-                $rows = (int) $_POST["n_rows_cols"];
-                if ($rows < 1 || $rows > 26) {
-                    $rowError = "n_rows_cols must be between 1 and 26!";
-                }
-            }
-
-            // load and check number of colors
-            if (isset($_POST["n_colors"])) {
-                $colors = (int) $_POST["n_colors"];
-                if ($colors < 1 || $colors > 10) {
-                    $colorError = "n_colors must be between 1 and 10!";
-                }
+        if (isset($_GET["n_rows_cols"])) {
+            $rows = (int) Input::get("n_rows_cols");
+            if ($rows < 1 || $rows > 26) {
+                $rowError = "n_rows_cols must be between 1 and 26!";
             }
         }
+
+        // load and check number of colors
+        if (isset($_GET["n_colors"])) {
+            $colors = (int) Input::get("n_colors");
+            if ($colors < 1 || $colors > 10) {
+                $colorError = "n_colors must be between 1 and 10!";
+            }
+        }
+        
         
 
         $this->template->content = View::forge("colorcoor.php");
         $this->template->title = "Color Coordinate";
         $this->template->content->rows = $rows;
         $this->template->content->colors = $colors;
-        $this->template->content->requestType = $requestMethod;
         $this->template->content->rowError = $rowError;
         $this->template->content->colorError = $colorError;
         $this->template->homeStyle = 'normal';
