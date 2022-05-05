d7e050fe9411716ca75777aba2435a609974abdd
Megan Booher <meganboo@rams.colostate.edu>
2022-05-05 15:46:15 -0600

    added coorindates to right side of table. Fixed color swap

---
+++ m1/fuel/app/logs/2022/05/04.php
@@ -0,0 +1,7 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+ERROR - 2022-05-04 16:03:22 --> Fatal Error - Allowed memory size of 134217728 bytes exhausted (tried to allocate 98566160 bytes) in /s/bach/m/under/meganboo/local_html/CS312_Term_Project/m1/fuel/app/views/colorcoor.php on line 31
+ERROR - 2022-05-04 16:06:20 --> Fatal Error - Allowed memory size of 134217728 bytes exhausted (tried to allocate 98566152 bytes) in /s/bach/m/under/meganboo/local_html/CS312_Term_Project/m1/fuel/app/views/colorcoor.php on line 31
+ERROR - 2022-05-04 16:06:30 --> Fatal Error - Allowed memory size of 134217728 bytes exhausted (tried to allocate 98566152 bytes) in /s/bach/m/under/meganboo/local_html/CS312_Term_Project/m1/fuel/app/views/colorcoor.php on line 31
+ERROR - 2022-05-04 16:06:39 --> Fatal Error - Allowed memory size of 134217728 bytes exhausted (tried to allocate 98566152 bytes) in /s/bach/m/under/meganboo/local_html/CS312_Term_Project/m1/fuel/app/views/colorcoor.php on line 28
+ERROR - 2022-05-04 16:06:57 --> Fatal Error - Allowed memory size of 134217728 bytes exhausted (tried to allocate 98566152 bytes) in /s/bach/m/under/meganboo/local_html/CS312_Term_Project/m1/fuel/app/views/colorcoor.php on line 30
